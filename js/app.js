34614232d4ad03c6ebefdfe065868a593e23f208
Solofo <solofo@precision.mg>
2018-02-20 18:42:23 +0300

    Fix

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ app.filter('age', function() {
 app.service('scopeService', function() {
     return {
         safeApply: function ($scope, fn) {
-            var phase = $scope.$root.$$phase;
+            const phase = $scope.$root.$$phase;
             if (phase === '$apply' || phase === '$digest') {
                 if (fn && typeof fn === 'function') {
                     fn();
